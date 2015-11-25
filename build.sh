4db9fd09d8f746a2926397182110e59c9f0cc3d5
rockinroyle <rockinroyle@gmail.com>
2015-11-24 20:38:15 -0500

    w/stock toolchain. Change to different scripts per toolchain (for now?)

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 # basic build script
 
+
 # Define variables
 
 ROM="$HOME/aospd"
@@ -12,16 +13,22 @@ DEFCON='aospd_defconfig'
 UBER_DIR="$HOME/toolchains/arm-eabi-4.8-cortex-a15"
 UBER_GCC="$HOME/toolchains/arm-eabi-4.8-cortex-a15/bin/arm-eabi-"
 DEFAULT_GCC="$ROM/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin/arm-eabi-"
+LOGS="$HOME/ROMlog"
+
+
+# Home base
+
+if [ ! -e "$LOGS" ]; then
+	mkdir $LOGS
+fi
+
 
 # Setup Cross compilation
 
 export ARCH="arm"
 export SUBARCH="arm"
+export CROSS_COMPILE="$DEFAULT_GCC"
 
-if [ -e "$UBER_DIR" ]; then
-	export CROSS_COMPILE="$UBER_GCC" && echo "Building kernel with UBER-4.8-arm-eabi-cortex-a15"
-else export CROSS_COMPILE="$DEFAULT_GCC" && echo "Defaulting to AOSP prebuilt arm-eabi toolchain"
-fi
 	
 # Start building
 
@@ -30,41 +37,42 @@ if [ -e "$KERNEL_SRC" ]; then
 	else echo "Directory doesn't exist? Check that out will ya? ;)";
 fi
 
-make clean
-make mrproper
-make $DEFCON
+make clean | tee $LOGS/kernelclean.txt
+
+if [ $? -eq 1 ]; then
+	echo "Error! Check ~/ROMlog/kernelclean.txt!"
+fi
+
+make mrproper | tee $LOGS/mrproper.txt
+
+if [ $? -eq 1 ]; then
+	echo "Error! Check ~/ROMlog/mrproper.txt!"
+fi
+
+make $DEFCON | tee $LOGS/kernelbuild.txt
+
+if [ $? -eq 1 ]; then
+	echo "Error! Check ~/ROMlog/kernelbuild.txt!"
+fi
+
 make -j4
 
 if [ $? -eq 1 ]; then
-	unset CROSS_COMPILE
-fi
-
-export CROSS_COMPILE="$DEFAULT_GCC" && echo "Defaulting to AOSP prebuilt arm-eabi toolchain"
-
-if [ -e "$KERNEL_SRC" ]; then
-	cd $KERNEL_SRC
-	else echo "Directory doesn't exist? Check that out will ya? ;)";
-fi
-
-make clean
-make mrproper
-make $DEFCON
-make -j4
-
- if [ $? -eq 1 ]; then
-	echo "Kernel compilation error. Build is stopping!"
-	exit
+	echo "Error! Check ~/ROMlog/kernelmake.txt!"\n
+	"Kernel compilation error. Build is stopping!";
+	exit;
 fi
 
 if [ -e "$KERNEL_LOC" ]; then
 	rm -R $KERNEL_LOC
-elif [ ! -e "$KERNEL_LOC" ]; then
-	mkdir -p $KERNEL_LOC
+	else mkdir -p $KERNEL_LOC
 fi
 
-if [ -n "$KERNEL_PRODUCT" ]; then
+if [ -e "$KERNEL_PRODUCT" ]; then
 	mv $KERNEL_PRODUCT $KERNEL_LOC/zImage-dtb;
-	else echo "Kernel build failed! Ending script!";
+	else mv $KERNEL_PRODUCT $KERNEL_LOC/zImage-dtb | tee $LOGS/buildlog.txt;
+	echo "Kernel build failed! Ending script! Check ~/ROMlog/buildlog.txt";
+	exit;
 fi
 
 cd $ROM
@@ -74,12 +82,12 @@ make clobber
 echo "Clean house!!!!!!!!"
 lunch aospd_shamu-userdebug
 echo "Well, well, well. Check out the big brain on Brad!"
-make -j4 otapackage | tee $HOME/buildlog.txt
+make -j4 otapackage | tee $LOGS/buildlog.txt
 
 if [ $? -eq 0 ]; then
-	echo "Build succesful!!!" `date`
-	exit
+	echo "Build succesful!!!" `date`;
+	exit;
 elif [ $? -ne 0 ]; then
-	echo "Ooopsy something went wrong!?!?"
-	exit
+	echo "Ooopsy something went wrong!?!?";
+	exit;
 fi
